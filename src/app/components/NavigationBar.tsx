2192c9e8d72b83e106cfd748ad12670985307acd
Dylan <dylanhudson1998@gmail.com>
2023-09-03 19:17:50 +0100

    Added basic navigationbar

---
+++ src/app/components/NavigationBar.tsx
@@ -0,0 +1,9 @@
+export function NavigationBar() {
+  return (
+    <nav className="w-full">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-center mx-auto p-4">
+        <div>InfoCaff</div>
+      </div>
+    </nav>
+  );
+}

be447609db0b7046ab946d7fd7be5d46169088ce
Dylan <dylanhudson1998@gmail.com>
2023-09-03 19:18:03 +0100

    Added custom 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,45 @@
+export default function NotFoundPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <div className="w-full h-screen flex flex-col lg:flex-row items-center justify-center space-y-16 lg:space-y-0 space-x-8 2xl:space-x-0">
+        <div className="w-full lg:w-1/2 flex flex-col items-center justify-center lg:px-2 xl:px-0 text-center">
+          <p className="text-7xl md:text-8xl lg:text-9xl font-bold tracking-wider text-gray-300">
+            404
+          </p>
+          <p className="text-4xl md:text-5xl lg:text-6xl font-bold tracking-wider text-gray-300 mt-2 my-12">
+            Page Not Found
+          </p>
+          <a
+            href="/"
+            className="flex items-center space-x-2 bg-blue-600 hover:bg-blue-700 text-gray-100 px-4 py-2 rounded transition duration-150"
+            title="Return Home"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M9.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L7.414 9H15a1 1 0 110 2H7.414l2.293 2.293a1 1 0 010 1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+            <span>Return Home</span>
+          </a>
+        </div>
+        <div className="w-1/2 lg:h-full flex lg:items-end justify-center">
+          <svg
+            className="w-full text-blue-600"
+            xmlns="http://www.w3.org/2000/svg"
+            data-name="Layer 1"
+            viewBox="0 0 1120.59226 777.91584"
+          >
+            <title>not found</title>
+          </svg>
+        </div>
+      </div>
+    </main>
+  );
+}

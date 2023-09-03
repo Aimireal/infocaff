2fe485540a6b56782eb77c9a2ee5f2aa010cb6f9
Dylan <dylanhudson1998@gmail.com>
2023-09-03 20:25:07 +0100

    Updated navbar title font layout

--- src/app/components/NavigationBar.tsx
+++ src/app/components/NavigationBar.tsx
@@ -2,7 +2,7 @@ export function NavigationBar() {
   return (
     <nav className="w-full">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-center mx-auto p-4">
-        <div>InfoCaff</div>
+        <div className="text-2xl font-semibold">InfoCaff</div>
       </div>
     </nav>
   );

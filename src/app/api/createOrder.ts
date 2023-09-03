a896d3df1a32f2a74c6342746857e5f3731ee046
Dylan <dylanhudson1998@gmail.com>
2023-09-03 22:51:08 +0100

    Added createOrder API function

---
+++ src/app/api/createOrder.ts
@@ -0,0 +1,14 @@
+import { doc, setDoc } from "firebase/firestore";
+import { firestore } from "../config/clientApp";
+import IOrder from "../types/IOrder";
+
+const createOrder = async (order: IOrder) => {
+  try {
+    order.id = Date.now().toString();
+    await setDoc(doc(firestore, "orders", order.id), { ...order });
+  } catch (error) {
+    console.log("Error adding order");
+  }
+};
+
+export default createOrder;

a57688681f7a24e15347539eacb34110935eb152
Dylan <dylanhudson1998@gmail.com>
2023-09-03 20:21:43 +0100

    Updated Order Card to align remove to end

--- src/app/components/OrderCard.tsx
+++ src/app/components/OrderCard.tsx
@@ -9,14 +9,27 @@ export default function OrderCard() {
           <p className="mb-2 text-sm text-gray-700 dark:text-gray-400">
             Milk Type
           </p>
-          <p className="mb-2 text-sm text-gray-700 dark:text-gray-400">
-            Syrup ~ Special Instruction
-          </p>
+          <div className="grid grid-cols-3">
+            <p className="mb-2 text-sm text-gray-700 dark:text-gray-400">
+              Syrup: N/A
+            </p>
+            <p className="mb-2 text-sm text-gray-700 dark:text-gray-400">
+              Sugar: N/A
+            </p>
+            <p className="mb-2 text-sm text-gray-700 dark:text-gray-400">
+              Special: N/A
+            </p>
+          </div>
         </div>
-        <div>
-          <button className="px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Remove
-          </button>
+        <div className="flex justify-end">
+          <div className="w-2/5 grid grid-cols-1">
+            <p className="mb-2 text-sm text-gray-700 dark:text-gray-400">
+              Order for
+            </p>
+            <button className="px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              Remove Order
+            </button>
+          </div>
         </div>
       </div>
     </div>

10d6364f0348ba0120d57c97cd00febc3980487b
Dylan <dylanhudson1998@gmail.com>
2023-09-05 22:49:59 +0100

    Updated 404 page display

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,8 @@
 export default function NotFoundPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <div className="w-full h-screen flex flex-col lg:flex-row items-center justify-center space-y-16 lg:space-y-0 space-x-8 2xl:space-x-0">
-        <div className="w-full lg:w-1/2 flex flex-col items-center justify-center lg:px-2 xl:px-0 text-center">
+      <div className="w-full h-screen flex flex-col items-center justify-center space-y-16 lg:space-y-0 space-x-8 2xl:space-x-0">
+        <div className="w-full mt-40 lg:w-1/2 flex flex-col items-center justify-center lg:px-2 xl:px-0 text-center">
           <p className="text-7xl md:text-8xl lg:text-9xl font-bold tracking-wider text-gray-300">
             404
           </p>

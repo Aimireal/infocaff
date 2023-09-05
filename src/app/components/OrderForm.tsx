014719580639070c6ad2549472870c4cf08d1c98
Dylan <dylanhudson1998@gmail.com>
2023-09-05 21:34:48 +0100

    Extracted OrderForm inputs to components

--- src/app/components/OrderForm.tsx
+++ src/app/components/OrderForm.tsx
@@ -5,19 +5,21 @@ import createOrder from "../api/createOrder";
 import { DrinkTypes } from "../data/DrinkTypes";
 import { MilkTypes } from "../data/MilkTypes";
 import { SizeTypes } from "../data/SizeTypes";
+import { OrderFormSelectInput } from "./OrderFormSelectInput";
+import { OrderFormTextInput } from "./OrderFormTextInput";
 
 export default function OrderForm() {
   const [addingOrder, setAddingOrder] = useState(false);
 
-  const [coffeeType, setCoffeeType] = useState("");
+  const [drinkType, setDrinkType] = useState("");
   const [milkType, setMilkType] = useState("");
   const [size, setSize] = useState("");
   const [specialInstructions, setSpecialInstructions] = useState("");
   const [orderFor, setOrderFor] = useState("");
 
-  const handleCoffeeTypeChange = (e: {
+  const handleDrinkTypeChange = (e: {
     target: { value: React.SetStateAction<string> };
-  }) => setCoffeeType(e.target.value);
+  }) => setDrinkType(e.target.value);
 
   const handleMilkTypeChange = (e: {
     target: { value: React.SetStateAction<string> };
@@ -36,21 +38,19 @@ export default function OrderForm() {
   }) => setOrderFor(e.target.value);
 
   const createIOrderObject = () => {
-    const specialInstructionsValue =
-      specialInstructions.trim() === "" ? "None" : specialInstructions;
-
     return {
-      drinkType: coffeeType,
+      drinkType: drinkType,
       milkType: milkType,
       drinkSize: size,
-      specialInstructions: specialInstructions,
+      specialInstructions:
+        specialInstructions.trim() === "" ? "None" : specialInstructions,
       orderFor: orderFor,
     };
   };
 
   const handleCreateOrderClick = async () => {
     if (
-      coffeeType === "" ||
+      drinkType === "" ||
       milkType === "" ||
       size === "" ||
       specialInstructions === "" ||
@@ -69,7 +69,7 @@ export default function OrderForm() {
   };
 
   const resetForm = () => {
-    setCoffeeType("");
+    setDrinkType("");
     setMilkType("");
     setSize("");
     setSpecialInstructions("");
@@ -80,71 +80,34 @@ export default function OrderForm() {
     <div className="w-full p-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <div>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4 m-2 justify-items-center">
-          <div>
-            <label className="block text-sm mb-2">Drink Type</label>
-            <select
-              id="drinkType"
-              className="w-60 text-sm rounded-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              required
-              onChange={handleCoffeeTypeChange}
-            >
-              <option value="">Select a Drink Type</option>
-              {DrinkTypes.map((type) => (
-                <option key={type} value={type}>
-                  {type}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div>
-            <label className="block text-sm mb-2">Milk Type</label>
-            <select
-              id="milkType"
-              className="w-60 text-sm rounded-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              required
-              onChange={handleMilkTypeChange}
-            >
-              <option value="">Select a Milk Type</option>
-              {MilkTypes.map((type) => (
-                <option key={type} value={type}>
-                  {type}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div>
-            <label className="block text-sm mb-2">Size</label>
-            <select
-              id="size"
-              className="w-60 text-sm rounded-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              required
-              onChange={handleSizeChange}
-            >
-              <option value="">Select a Size</option>
-              {SizeTypes.map((type) => (
-                <option key={type} value={type}>
-                  {type}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div>
-            <label className="block text-sm mb-2">Special Instructions</label>
-            <input
-              id="specialInstructions"
-              className="w-60 text-sm rounded-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              onChange={handleSpecialInstructionsChange}
-            />
-          </div>
-          <div>
-            <label className="block text-sm mb-2">Order For</label>
-            <input
-              id="orderFor"
-              className="w-60 text-sm rounded-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              required
-              onChange={handleOrderForChange}
-            />
-          </div>
+          <OrderFormSelectInput
+            label="Drink Type"
+            options={DrinkTypes}
+            onChange={handleDrinkTypeChange}
+          />
+          <OrderFormSelectInput
+            label="Milk Type"
+            options={MilkTypes}
+            onChange={handleMilkTypeChange}
+          />
+          <OrderFormSelectInput
+            label="Size"
+            options={SizeTypes}
+            onChange={handleSizeChange}
+          />
+          <OrderFormSelectInput
+            label="Size"
+            options={SizeTypes}
+            onChange={handleSizeChange}
+          />
+          <OrderFormTextInput
+            label="Special Instructions"
+            onChange={handleSpecialInstructionsChange}
+          />
+          <OrderFormTextInput
+            label="Order For"
+            onChange={handleOrderForChange}
+          />
         </div>
       </div>
       <div className="flex justify-center mt-5">
---
+++ src/app/components/OrderFormTextInput.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { IOrderInputProps } from "../types/IOrderInputProps";
+
+export function OrderFormTextInput({ label, onChange }: IOrderInputProps) {
+  return (
+    <div>
+      <label className="block text-sm mb-2">{label}</label>
+      <input
+        id="orderFormSelectInput"
+        className="w-60 text-sm rounded-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        required
+        onChange={onChange}
+      ></input>
+    </div>
+  );
+}

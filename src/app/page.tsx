75647171c4e959e041cdebb73d1fef65fb078f60
Dylan <dylanhudson1998@gmail.com>
2023-09-09 00:08:01 +0100

    Added conditional rendering to the OrderForm for mobile screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,32 @@
+"use client";
+
 import FullscreenModal from "./components/FullscreenModal";
 import OrderForm from "./components/orderForm/OrderForm";
 import OrdersGrid from "./components/OrdersGrid";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkIsMobile = () => {
+      setIsMobile(window.innerWidth < 640);
+    };
+
+    checkIsMobile();
+    window.addEventListener("resize", checkIsMobile);
+
+    return () => {
+      window.removeEventListener("resize", checkIsMobile);
+    };
+  }, []);
   return (
     <main className="flex flex-col items-center p-18">
-      <FullscreenModal>
-        {<OrderForm />}
-      </FullscreenModal>
+      {isMobile ? (
+        <FullscreenModal>{<OrderForm />}</FullscreenModal>
+      ) : (
+        <OrderForm />
+      )}
       <OrdersGrid />
       <div className="w-5/6 grid grid-cols-1 gap-4"></div>
     </main>

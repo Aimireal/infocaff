a0431154ba6765f60ae850841f51fb3bd1da74a7
Dylan <dylanhudson1998@gmail.com>
2023-09-10 19:02:38 +0100

    Reworked Firebase API calls

--- src/app/firebase/firebase.ts
+++ src/app/firebase/firebase.ts
@@ -1,7 +1,10 @@
-import { getApps, initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { initializeFirestore } from "firebase/firestore";
 
 const apiKey = process.env.NEXT_PUBLIC_FIREBASE_API_KEY;
 const authDomain = process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN;
+const databaseURL = process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL;
 const projectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
 const storageBucket = process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET;
 const messagingSenderId = process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID;
@@ -10,12 +13,17 @@ const appId = process.env.NEXT_PUBLIC_FIREBASE_APP_ID;
 const firebaseConfig = {
   apiKey,
   authDomain,
+  databaseURL,
   projectId,
   storageBucket,
   messagingSenderId,
   appId,
 };
 
-let firebaseApp = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
+const db = initializeFirestore(firebaseApp, {
+  experimentalForceLongPolling: true,
+});
 
-export { firebaseApp };
+export { auth, db };

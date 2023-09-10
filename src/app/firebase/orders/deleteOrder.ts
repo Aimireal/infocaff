13d8a759e9bbfa001e7b5f4037330accca67ee00
Dylan <dylanhudson1998@gmail.com>
2023-09-10 11:02:31 +0100

    Refactored Firebase API integration and CRUD operations for Orders
    # Conflicts:
    #	src/app/components/orderForm/OrderForm.tsx

--- src/app/firebase/orders/deleteOrder.ts
+++ src/app/firebase/orders/deleteOrder.ts
@@ -1,5 +1,5 @@
 import { deleteDoc, doc } from "firebase/firestore";
-import { firestore } from "../config/clientApp";
+import { firestore } from "./firestore";
 
 export const deleteOrder = async (documentId: string) => {
   const _order = doc(firestore, `orders/${documentId}`);
--- src/app/firebase/orders/updateOrder.ts
+++ src/app/firebase/orders/updateOrder.ts
@@ -1,5 +1,5 @@
 import { doc, updateDoc } from "firebase/firestore";
-import { firestore } from "../config/clientApp";
+import { firestore } from "../firebase/orders/firestore";
 
 export const updateOrder = async (documentId: string) => {
   const _order = doc(firestore, `orders/${documentId}`);

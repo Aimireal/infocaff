0662d049934e76a18e482c18d0a5d6e00734752a
Dylan <dylanhudson1998@gmail.com>
2023-09-10 21:14:33 +0100

    Added return home to navbar

--- src/app/components/NavigationBar.tsx
+++ src/app/components/NavigationBar.tsx
@@ -3,8 +3,10 @@
 import React, { useState, useEffect } from "react";
 import { useModal } from "../context/ModalProvider";
 import { NavbarAvatar } from "./NavbarAvatar";
+import { useRouter } from "next/navigation";
 
 export function NavigationBar() {
+  const router = useRouter();
   const { openModal } = useModal();
   const [isMobile, setIsMobile] = useState(false);
 
@@ -21,10 +23,17 @@ export function NavigationBar() {
     };
   }, []);
 
+  const returnHome = () => {
+    router.push("/");
+  };
+
   return (
     <nav className="flex justify-between items-center p-4">
       <div className="flex-none w-1/3"></div>
-      <div className="flex-grow text-2xl font-semibold text-center">
+      <div
+        onClick={returnHome}
+        className="flex-grow text-2xl font-semibold text-center"
+      >
         InfoCaff
       </div>
       <div className="flex items-center flex-none w-1/3 justify-end">
@@ -33,7 +42,7 @@ export function NavigationBar() {
             Place Order
           </button>
         )}
-        <NavbarAvatar/>
+        <NavbarAvatar />
       </div>
     </nav>
   );

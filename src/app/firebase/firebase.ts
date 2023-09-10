804b537cc30e26bcad3b035279401d0d73132e69
Dylan <dylanhudson1998@gmail.com>
2023-09-10 20:36:53 +0100

    Updated AuthContext and added SignOut

--- src/app/firebase/firebase.ts
+++ src/app/firebase/firebase.ts
@@ -11,13 +11,13 @@ const messagingSenderId = process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID;
 const appId = process.env.NEXT_PUBLIC_FIREBASE_APP_ID;
 
 const firebaseConfig = {
-  apiKey,
-  authDomain,
-  databaseURL,
-  projectId,
-  storageBucket,
-  messagingSenderId,
-  appId,
+  apiKey: "AIzaSyCfLRBWmrmuZJhKZK3q87IDbk5FqMv_-bk",
+  authDomain: "infocaff-d871e.firebaseapp.com",
+  databaseURL: "https://infocaff-d871e-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "infocaff-d871e",
+  storageBucket: "infocaff-d871e.appspot.com",
+  messagingSenderId: "320564662684",
+  appId: "1:320564662684:web:85b551a42e48f2f38b7859"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);

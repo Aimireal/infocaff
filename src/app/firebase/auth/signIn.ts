38b36e5a0ad5532abb14db622e5224cc989bc6f9
Dylan <dylanhudson1998@gmail.com>
2023-09-10 11:10:12 +0100

    Added API calls for firebase authorisation

---
+++ src/app/firebase/auth/signIn.ts
@@ -0,0 +1,17 @@
+import { firebaseApp } from "../firebase";
+import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
+
+const auth = getAuth(firebaseApp);
+
+export default async function signIn(email: string, password: string) {
+  let result = null,
+    error = null;
+
+  try {
+    result = await signInWithEmailAndPassword(auth, email, password);
+  } catch (e) {
+    error = e;
+  }
+
+  return { result, error };
+}
---
+++ src/app/firebase/auth/signUp.ts
@@ -0,0 +1,15 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "./auth";
+
+export default async function signUp(email: string, password: string) {
+  let result = null;
+  let error = null;
+
+  try {
+    result = await createUserWithEmailAndPassword(auth, email, password);
+  } catch (e) {
+    error = e;
+  }
+
+  return { result, error };
+}
